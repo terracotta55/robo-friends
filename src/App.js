6e3433acb773ac32214a4c418c8f012d2a4f9803
Dalton Okechukwu <chike911@yahoo.com>
2019-11-10 13:52:05 -0700

    added state management for app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { robots } from "./robots";
 import { CardList } from "./CardList";
 import { SearchBox } from "./SearchBox";
 const Fragment = React.Fragment;
 
-export const App = () => {
-	return (
-		<Fragment>
-			<div className="tc">
-				<h1>Robot Friends</h1>
-				<SearchBox />
-				<CardList robots={robots} />
-			</div>
-		</Fragment>
-	);
-};
+export class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: ""
+		};
+	}
+	onSearchChange = e => {
+        this.setState({searchfield: e.target.value})
+	}
+	render() {
+        const filteredRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()))
+        console.log(filteredRobots);
+		return (
+			<Fragment>
+				<div className="tc">
+					<h1>Robot Friends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<CardList robots={filteredRobots} />
+				</div>
+			</Fragment>
+		);
+	}
+}
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,11 @@
 import React from "react";
 const Fragment = React.Fragment;
 
-export const SearchBox = () => {
+export const SearchBox = ({ searchChange }) => {
 	return (
 		<Fragment>
 			<div className="pa2">
-				<input className="pa2 bg-lightest-blue ba b--green br2 bw1 shadow-3" type="search" placeholder="search robots" />
+				<input className="pa2 bg-lightest-blue ba b--green br2 bw1 shadow-3" type="search" placeholder="search robots" onChange ={searchChange} />
 			</div>
 		</Fragment>
 	);
