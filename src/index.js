ceb121ac666c227463922d7124fd2318aa1d6b5e
Dalton Okechukwu <chike911@yahoo.com>
2019-11-10 10:44:15 -0700

    added card component and props

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import { Card } from "./Card";
+import "tachyons";
+import { robots } from "./robots";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+	</div>,
+	document.getElementById("root")
+);
+// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<h1>Robo Friends App</h1>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
