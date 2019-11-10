7c4a6920d6aa8f616b8bced100f1ade73b64078d
Dalton Okechukwu <chike911@yahoo.com>
2019-11-10 15:32:31 -0700

    added tachyons bw update on classname

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Fragment = React.Fragment;
 export const Card = ({ id, name, email }) => {
 	return (
 		<Fragment>
-			<div className="tc bg-moon-gray dib br4 pa2 ma2 grow ba b--green bw1 shadow-3">
+			<div className="tc bg-moon-gray dib br4 pa2 ma2 grow ba b--yellow bw2 shadow-3">
 				<img alt="robots-pic" src={`https://robohash.org/${id}?200x200`} />
 				<div>
 					<h2>{name}</h2>
