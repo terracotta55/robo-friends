0feaaaaa079f333c92aa6e05f35b448b477d466b
Dalton Okechukwu <chike911@yahoo.com>
2019-11-10 17:11:10 -0700

    Updates

---
+++ precache-manifest.056733b815a20aabe714f146f84f91c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9f3b6850344b0b23e57140c4eab825",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "12b14603e85f477c5ead",
+    "url": "/robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "715cee72c3216856904b",
+    "url": "/robo-friends/static/css/main.05563c9a.chunk.css"
+  },
+  {
+    "revision": "12b14603e85f477c5ead",
+    "url": "/robo-friends/static/js/2.69962bad.chunk.js"
+  },
+  {
+    "revision": "715cee72c3216856904b",
+    "url": "/robo-friends/static/js/main.14a21014.chunk.js"
+  },
+  {
+    "revision": "034da7a34efd67e8ff4e",
+    "url": "/robo-friends/static/js/runtime-main.f31c0b1b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
